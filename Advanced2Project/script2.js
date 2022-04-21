88e4796c8bc5e23b0126ab91bab9645c7eb5b995
KareenSalameh <itskareen3@hotmail.com>
2022-04-22 00:38:42 +0300

    signUp

---
+++ Advanced2Project/script2.js
@@ -0,0 +1,31 @@
+function validication() {
+
+    
+    var username=document.getElementById("user").value;
+    var nickn=document.getElementById("nickname").value;
+    var psw=document.getElementById("password").value;
+        let user = document.getElementById('user');
+        let pass = document.getElementById('password');
+        let user_records=new Array();
+        user_records=JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[]
+        if (user.value.length <= 8 && user.value.length >= 2) {} else {
+            alert("Username has to be between 2-8 characters.")
+        }
+        if (pass.value.length <= 8 && pass.value.length >= 2) {} else {
+            alert("Password has to be between 2-8 characters.")
+        }
+      
+        if(user_records.some((v)=>{return v.user==user.value}))
+        {
+            alert('The user already exist')
+        }
+        else{
+          user_records.push({
+            "user":username,
+            "nickname":nickn,
+            "password":psw
+          })
+        localStorage.setItem("users",JSON.stringify(user_records)); 
+       }
+            return false;
+}
